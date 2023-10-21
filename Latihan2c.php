52dc2cd0d5ab5ee45aa3c752221b320ca2addc52
Khoirunnajwa <148507944+Khoirunnajwa@users.noreply.github.com>
2023-10-22 05:27:48 +0700

    Latihan2c.php

--- Latihan2c.php
+++ Latihan2c.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Latihan 1c</title>
+    <title>Latihan 2b dan 2c </title>
     <style>
         .boxed-letter {
             background-color: lightgray;
